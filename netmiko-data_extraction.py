79510d897aee77a42dca66d27d36090f3a86d5b2
Jorge Marcano <jamg1700@gmail.com>
2021-05-31 12:44:26 -0400

    improvements on data extraction

--- netmiko-data_extraction.py
+++ netmiko-data_extraction.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+#v1.1.0
+
 import os, getpass, sys, re
 from datetime import datetime
 from netmiko import ConnectHandler
@@ -18,14 +21,93 @@ list_of_speeds = []
 list_of_duplex = []
 list_of_modes = []
 list_of_description = []
+list_of_states = []
 list_of_host = []
 list_of_sw = []
+list_of_channel = []
+list_of_sfp = []
 total_sw = len(sw)
 tiempo1 = datetime.now()
 tiempo_inicial = tiempo1.strftime("%H:%M:%S")
 print(f"La ejecucion de este programa inicio a las {tiempo_inicial}, se validara un total de {str(total_sw)} switch.")
 
 def data_extraction(conn,inter,list_of_uplinks,hostname):
+    if sw_i in "": #NEXUS_OS
+        state = conn.send_command("show interface %s | include down" % inter)
+        if "is down" in state:
+            state_0 = re.search(r"\(\D+\)",state)
+            if state_0 == None:
+                list_of_states.append("(down)")
+            else:
+                state = state_0.group()
+                list_of_states.append(state)
+        else:
+            list_of_states.append("(connected)")
+        if "Po" not in inter:
+            channel = conn.send_command("show running-config interface %s | include channel" % inter)
+            port_0 = re.search(r"\d+",channel)
+            if port_0 != None:
+                port = port_0.group()
+                if "auto" in channel or "desirable" in channel:
+                    channel_type = "LACP"
+                elif "active" in channel or "passive" in channel:
+                    channel_type = "PAgP"
+                elif "on" in channel:
+                    channel_type = "on"
+                channel = "Po"+ str(port) + " "+ channel_type
+                list_of_channel.append(channel)
+            elif port_0 == None:
+                list_of_channel.append("Interfaz standalone")
+        elif "Po" in inter:
+            channel = conn.send_command("show port-channel summary | include %s" % inter)
+            port_0 = re.search(r"Po\d+\(\w+\)",channel)
+            port = port_0.group()
+            if "LACP" in channel:
+                channel_type = "LACP"
+            elif "PAgP"in channel:
+                channel_type = "PAgP"
+            else:
+                channel_type = "on"
+            channel = port + " "+ channel_type
+            list_of_channel.append(channel)
+    else: #IOS
+        state = conn.send_command("show interface %s | include line" % inter)
+        if "is up" in state:
+            list_of_states.append("(connected)")
+        else:
+            state_0 = re.search(r"\(\D+\)",state)
+            if state_0 == None:
+                list_of_states.append("(down)")
+            else:
+                state = state_0.group()
+                list_of_states.append(state)
+        if "Po" not in inter:
+            channel = conn.send_command("show running-config interface %s | include channel" % inter)
+            port_0 = re.search(r"\d+",channel)
+            if port_0 != None:
+                port = port_0.group()
+                if "auto" in channel or "desirable" in channel:
+                    channel_type = "LACP"
+                elif "active" in channel or "passive" in channel:
+                    channel_type = "PAgP"
+                elif "on" in channel:
+                    channel_type = "on"
+                channel = "Po"+ str(port) + " "+ channel_type
+                list_of_channel.append(channel)
+            elif port_0 == None:
+                list_of_channel.append("Interfaz standalone")
+        elif "Po" in inter:
+            channel = conn.send_command("show etherchannel summary | include %s" % inter)
+            port_0 = re.search(r"Po\d+\(\w+\)",channel)
+            port = port_0.group()
+            if "LACP" in channel:
+                channel_type = "LACP"
+            elif "PAgP"in channel:
+                channel_type = "PAgP"
+            else:
+                channel_type = "on"
+            channel = port + " "+ channel_type
+            list_of_channel.append(channel)
     list_of_host.append(hostname.strip("#"))
     list_of_sw.append(sw_i)
     if inter in list_of_uplinks:
@@ -38,8 +120,18 @@ def data_extraction(conn,inter,list_of_uplinks,hostname):
         elif mac_int != []:
             list_of_mac.append(mac_int)
     list_of_int.append(inter)
-    trunk = conn.send_command("show interface %s status" % inter)
-    if "trunk" in trunk:
+    status = conn.send_command("show interface %s status" % inter)
+    if "aseT" in status or "-T" in status:
+        list_of_sfp.append("TX")
+    elif "base-SR" in status:
+        list_of_sfp.append("SR")
+    elif "base-SX" in status or "BaseSX" in status:
+        list_of_sfp.append("SX")
+    elif "Po" in inter:
+        list_of_sfp.append("Port-channel")
+    else:
+        list_of_sfp.append("Not Present")
+    if "trunk" in status:
         list_of_modes.append("trunk")
         running_config = conn.send_command("show running-config interface %s | in allowed" % inter)
         vlan = re.findall(r"\d+", running_config)
@@ -47,7 +139,7 @@ def data_extraction(conn,inter,list_of_uplinks,hostname):
             list_of_vlan.append(vlan)
         elif vlan == []:
             list_of_vlan.append("Interfaz sin VLAN")
-    elif "trunk" not in trunk:
+    elif "trunk" not in status:
         list_of_modes.append("access")
         running_config = conn.send_command("show running-config interface %s | in access" % inter)
         vlan = re.findall(r"\d+", running_config)
@@ -55,12 +147,11 @@ def data_extraction(conn,inter,list_of_uplinks,hostname):
             list_of_vlan.append(vlan)
         elif vlan == []:
             list_of_vlan.append("Interfaz sin VLAN")
-    duplex = conn.send_command("show interface %s status" % inter)
-    if "full" in duplex:
+    if "full" in status:
         list_of_duplex.append("Full")
-    elif "half" in duplex:
+    elif "half" in status:
         list_of_duplex.append("Half")
-    elif "auto" in duplex:
+    elif "auto" in status:
         list_of_duplex.append("Auto")
     speed_str = conn.send_command("show interface %s | include BW" % inter)
     bw_0 = re.search(r"BW \d+", speed_str)
