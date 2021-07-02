824af51e5190848d0d8625ec843a3bbe5e120c2a
Jorge Marcano <jamg1700@gmail.com>
2021-07-02 02:02:58 -0400

    fixes to port-channel detection

--- netmiko-data_extraction.py
+++ netmiko-data_extraction.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#v1.1.0
+#v1.1.1
 
 import os, getpass, sys, re
 from datetime import datetime
@@ -48,9 +48,9 @@ def data_extraction(conn,inter,list_of_uplinks,hostname):
             port_0 = re.search(r"\d+",channel)
             if port_0 != None:
                 port = port_0.group()
-                if "auto" in channel or "desirable" in channel:
+                if "active" in channel or "passive" in channel:
                     channel_type = "LACP"
-                elif "active" in channel or "passive" in channel:
+                elif "auto" in channel or "desirable" in channel:
                     channel_type = "PAgP"
                 elif "on" in channel:
                     channel_type = "on"
@@ -86,9 +86,9 @@ def data_extraction(conn,inter,list_of_uplinks,hostname):
             port_0 = re.search(r"\d+",channel)
             if port_0 != None:
                 port = port_0.group()
-                if "auto" in channel or "desirable" in channel:
+                if "active" in channel or "passive" in channel:
                     channel_type = "LACP"
-                elif "active" in channel or "passive" in channel:
+                elif "auto" in channel or "desirable" in channel:
                     channel_type = "PAgP"
                 elif "on" in channel:
                     channel_type = "on"
