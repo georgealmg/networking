33c036654960646ca668a9f2a218301090cc385e
Jorge Marcano <jamg1700@gmail.com>
2021-08-28 08:12:53 -0400

    Organizing the code

--- netmiko_backup.py
+++ netmiko_backup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#v1.0.1
+#v1.0.2
 
 import os, concurrent.futures, sys
 from openpyxl import load_workbook
@@ -8,30 +8,28 @@ from datetime import datetime
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException, SSHException, AuthenticationException
 
-def DC(sw,user,pas,sw_out):
-    def respaldos_DC(conn):
-        hostname = (conn.find_prompt())
-        respaldos_txt = open(hostname.strip("#")+".txt","+w")
-        respaldos_txt.write(conn.send_command("show running-config"))
-        respaldos_txt.close()
-        try:
-            conn.save_config()
-        except(OSError):
-            conn.save_config(confirm=True,confirm_response="yes\r")
-        conn.disconnect()
-        print(f"Configuracion de {hostname} respaldada.")
+def backup(sw,user,pas,sw_out):
     try:
         conn = ConnectHandler(device_type= "cisco_ios_ssh",host= sw,username= user,password= pas)
-        respaldos_DC(conn)
     except(AuthenticationException):
         sys.exit("Por favor ejecute nuevamente el programa ya que introdujo una contraseña erronea.")
     except(NetMikoTimeoutException,SSHException):
         try:
             conn = ConnectHandler(device_type= "cisco_ios_telnet",host= sw,username= user,password= pas)
-            respaldos_DC(conn)
         except:
-            print(f"Switch con IP: {str(sw)} esta fuera de linea.")
+            print(f"Switch con IP \"{sw}\" esta fuera de linea.")
             sw_out.append(sw)
+            return None
+    hostname = conn.find_prompt()
+    respaldos_txt = open(hostname.strip("#")+".txt","+w")
+    respaldos_txt.write(conn.send_command("show running-config"))
+    respaldos_txt.close()
+    try:
+        conn.save_config()
+    except(OSError):
+        conn.save_config(confirm=True,confirm_response="yes\r")
+    conn.disconnect()
+    print(f"{hostname} configuracion respaldada.")
 
 def main():
     windowsuser = os.getlogin()
@@ -41,9 +39,9 @@ def main():
         os.chdir(os.getcwd())
     user = input("Username: ")
     pas = getpass()
+
     sw_dc = []
     sw_out = []
-    fp = open("sw_out.txt","+w")
     excel_file = load_workbook("Respaldos.xlsx")
     sheet = excel_file["IP"]
     for ip in range(2, 999999):
@@ -54,11 +52,11 @@ def main():
             break
     tiempo1 = datetime.now()
     tiempo_inicial = tiempo1.strftime("%H:%M:%S")
-    print(f"La ejecucion de este programa inicio a las {tiempo_inicial}, se validara un total de {len(sw_dc)} switch.")
+    print(f"La ejecucion de este programa inicio a las {tiempo_inicial}, se respaldara la configuracion de {len(sw_dc)} equipos.")
     
     fecha = datetime.now()
     directorio_fecha = fecha.strftime("%d-%m-%y")
-    directorio = "Data_Center"
+    directorio = "Data Center"
     try:
         os.mkdir(f"C:/Users/{windowsuser}/Documents/{directorio}")
     except(FileExistsError):
@@ -70,16 +68,17 @@ def main():
     os.chdir(f"C:/Users/{windowsuser}/Documents/{directorio}/{directorio_fecha}")
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor_nx:
-        ejecucion_nx = {executor_nx.submit(DC,sw,user,pas,sw_out,directorio,directorio_fecha): sw for sw in sw_dc}
+        ejecucion_nx = {executor_nx.submit(backup,sw,user,pas,sw_out,directorio,directorio_fecha): sw for sw in sw_dc}
     for output_nx in concurrent.futures.as_completed(ejecucion_nx):
         output_nx.result()
 
-    print(f"Equipos caidos: {sw_out}")
-    fp.write("Equipos caidos:"+"\n")
-    fp.close()
+    with open("sw_out.txt","+w") as file:
+        for sw in sw_out:
+            file.write(sw)
+
     tiempo2 = datetime.now()
     tiempo_final = tiempo2.strftime("%H:%M:%S")
-    print(f"La ejecucion de este programa finalizo a las {tiempo_final}, se valido un total de {len(sw_dc)} switch de los cuales {len(sw_out)} estan fuera de linea.")
+    print(f"La ejecucion de este programa finalizo a las {tiempo_final}, se respaldo la configuracion de {len(sw_dc)} equipos, {len(sw_out)} de los equipos estan fuera de linea.")
     tiempo_ejecucion = tiempo2 - tiempo1
     print(f"El tiempo de ejecucion del programa fue de: {tiempo_ejecucion}")
 
--- netmiko_config.py
+++ netmiko_config.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python3
-#v1.0.1
+#v1.0.2
 
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import SSHException, AuthenticationException, NetmikoTimeoutException
 from datetime import datetime
 import getpass, os, sys, concurrent.futures
 
-def NXOS(sw_n,user,pas,fp,fp1,comandos_nx):
-    def rutas(conn):
-        hostname = conn.find_prompt()
-        print(f"Ejecucion iniciada en {hostname} : {sw_n}")
-        output = conn.send_config_set(comandos_nx)
-        fp.write(f"{hostname} {sw_n}\n{output}\n")
-        clock = conn.send_command("show clock")
-        run_config = conn.send_command("show running-config")
-        fp1.write(f"{hostname} {sw_n}\n{clock}\n{run_config}")
-        print(f"Ejecucion finalizada en {hostname} : {sw_n}")
-        conn.disconnect()
+def config(sw,user,pas,fp,fp1,comandos_nx,out_count):
     try:
-        conn = ConnectHandler(device_type= "cisco_nxos_ssh",host= sw_n,username= user,password= pas)
-        rutas(conn)
+        conn = ConnectHandler(device_type= "cisco_nxos_ssh",host= sw,username= user,password= pas)
     except(AuthenticationException):
         sys.exit("Por favor ejecute nuevamente el programa ya que introdujo una contraseña erronea.")
     except(SSHException, NetmikoTimeoutException):
-        print(f"Switch con IP: {sw_n} esta fuera de linea")
+        out_count += 1
+        fp1.write(sw+"\n")
+        print(f"Switch con IP \"{sw}\" esta fuera de linea")
+        return None
+    hostname = conn.find_prompt()
+    print(f"Ejecucion iniciada en {hostname} : {sw}")
+    output = conn.send_config_set(comandos_nx)
+    fp.write(f"{hostname} {sw}\n{output}\n")
+    print(f"Ejecucion finalizada en {hostname} : {sw}")
+    conn.save_config()
+    conn.disconnect()
 
 def main():
-
+    
     windowsuser = os.getlogin()
-    os.chdir("C:/Users/"+windowsuser+"/Documents")
+    try:
+        os.chdir("C:/Users/"+str(windowsuser)+"/Documents")
+    except(FileNotFoundError):
+        os.chdir(os.getcwd())
     user = input("Username: ")
     pas = getpass.getpass()
 
-    sw_nx = []
+    sw_nxos = []
     comandos_nx = []
+    out_count = 0
     fp = open("config.txt","w+")
-    fp1 = open("respaldos.txt","w+")
+    fp1 = open("sw_out.txt","w+")
     tiempo1 = datetime.now()
     tiempo_inicial = tiempo1.strftime("%H:%M:%S")
-    print(f"La ejecucion de este programa inicio a las {tiempo_inicial}, se validara un total de {str(len(sw_nx))} switch.")
+    print(f"La ejecucion de este programa inicio a las {tiempo_inicial}, se validara un total de {str(len(sw_nxos))} switch.")
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor_nx:
-        ejecucion_nx = {executor_nx.submit(NXOS,sw_n,user,pas,fp,fp1,comandos_nx,): sw_n for sw_n in sw_nx}
+        ejecucion_nx = {executor_nx.submit(config,sw,user,pas,fp,fp1,comandos_nx,out_count): sw for sw in sw_nxos}
     for output_nx in concurrent.futures.as_completed(ejecucion_nx):
         output_nx.result()
 
@@ -49,9 +52,9 @@ def main():
     fp1.close()
     tiempo2 = datetime.now()
     tiempo_final = tiempo2.strftime("%H:%M:%S")
-    print(f"La ejecucion de este programa finalizo a las {tiempo_final}, se validaro un total de {str(len(sw_nx))}")
+    print(f"La ejecucion de este programa finalizo a las {tiempo_final}, se configurara un total de {str(len(sw_nxos))} equipos")
     tiempo_ejecucion = tiempo2 - tiempo1
-    print(f"El tiempo de ejecucion del programa fue de: {tiempo_ejecucion}")
+    print(f"El tiempo de ejecucion del programa fue de {tiempo_ejecucion}, {str(out_count)} de los equipos no pudieron ser configurados")
 
 if __name__ == "__main__":
     main()
