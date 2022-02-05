28e9921a855f75ffdfc199d5d437422e1d807f6d
Jorge Marcano <jamg1700@gmail.com>
2022-02-05 19:15:36 -0300

    move the scripts to a new folder

---
+++ newtorkdb/l2data.py
@@ -0,0 +1,190 @@
+#!/usr/bin/ python3
+#v1.0.0
+
+import concurrent.futures, json, socket
+from datetime import datetime
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetmikoTimeoutException, SSHException, AuthenticationException
+from ntc_templates.parse import parse_output
+
+acc = {"nxos":[],"ios":[]}
+l2dict = {"nxos":{},"ios":{}}
+user ,password = None, None
+
+# El objetivo de este script es la extraccion de las tablas ARP de los distintos core de la red.
+
+# Por medio de esta funcion se extrae las MAC de los equipos.
+
+def data(conn,device):
+
+    if device in acc["nxos"]:
+        env = "nxos"
+    elif device in acc["ios"]:
+        env = "ios"
+    
+    uplinks = []
+    if device in acc["ios"]:
+        channels = parse_output(platform="cisco_ios",command="show etherchannel summary",data=conn.send_command("show etherchannel summary"))
+        cdp = parse_output(platform="cisco_ios",command="show cdp neighbors",data=conn.send_command("show cdp neighbors"))
+    elif device in acc["nxos"]:
+        channels = parse_output(platform="cisco_nxos",command="show port-channel summary",data=conn.send_command("show port-channel summary"))
+        cdp = parse_output(platform="cisco_nxos",command="show cdp neighbors",data=conn.send_command("show cdp neighbors"))
+    hostname = conn.find_prompt()
+    print(f"Extrayendo data l2 --> {hostname}")
+    l2dict[env][hostname] = {}
+    l2dict[env][hostname]["interfaces"] = {}
+    l2dict[env][hostname]["swip"] = device
+
+    # El objetivo de este bloque de codigo es el de determinar cuales son interfaces de interconexion entre SW, de esta forma estas no se tomaran en cuenta para 
+    # la data requerida.
+
+    for cdpinter in cdp:
+        if "H" not in cdpinter["capability"]:
+            if "Fas" in cdpinter["local_interface"]:
+                uplink = cdpinter["local_interface"].replace("Fas ","Fa")
+            elif "Gig" in cdpinter["local_interface"]:
+                uplink = cdpinter["local_interface"].replace("Gig ","Gi")
+            elif "Ten" in cdpinter["local_interface"]:
+                uplink = cdpinter["local_interface"].replace("Ten ","Te")
+            elif "Eth" in cdpinter["local_interface"]:
+                uplink = cdpinter["local_interface"].replace("Eth ","Eth")
+            else:
+                pass
+            try:
+                uplinks.append(uplink)
+            except(UnboundLocalError,NameError):
+                pass
+        elif "H" in cdpinter["capability"]:
+            pass
+    
+    # La data obtenida se almacena en un diccionario, este se divide en ambientes y a su vez en los equipos de cada ambiente.
+    # de cada equipo se toma en cuenta si direccion IP y las interfaces con MAC
+
+    if device in acc["ios"]:
+        for channelsentry in channels:
+            for physinter in channelsentry["interfaces"]:
+                if physinter in uplinks:
+                    uplinks.append(channelsentry["po_name"])
+        vlanmac = parse_output(platform="cisco_ios",command="show mac address-table",data=conn.send_command("show mac address-table"))
+        for macentry in vlanmac:
+            dstport = macentry["destination_port"][0]
+            if "Port-channel" in dstport:
+                dstport = macentry["destination_port"][0].replace("Port-channel","Po")
+            elif "FastEthernet" in dstport:
+                dstport = macentry["destination_port"][0].replace("FastEthernet","Fa")
+            elif "GigabitEthernet" in dstport:
+                dstport = macentry["destination_port"][0].replace("GigabitEthernet","Gi")
+            if dstport not in uplinks and ("CPU" not in dstport and "Switch" not in dstport):
+                if dstport not in l2dict[env][hostname]["interfaces"]: 
+                    l2dict[env][hostname]["interfaces"][dstport] = {}
+                    l2dict[env][hostname]["interfaces"][dstport][macentry["destination_address"]] = "Vlan" + macentry["vlan"]
+                elif dstport in l2dict[env][hostname]["interfaces"]: 
+                    l2dict[env][hostname]["interfaces"][dstport][macentry["destination_address"]] = "Vlan" + macentry["vlan"]
+    elif device in acc["nxos"]:
+        for channelsentry in channels:
+            for physinter in channelsentry["phys_iface"]:
+                if physinter in uplinks:
+                    uplinks.append(channelsentry["bundle_iface"])
+        vlanmac = parse_output(platform="cisco_nxos",command="show mac address-table",data=conn.send_command("show mac address-table"))
+        for macentry in vlanmac:
+            if "vPC" in macentry["ports"] or "sup-eth" in macentry["ports"]:
+                pass
+            elif "vPC" not in macentry["ports"] or "sup-eth" not in macentry["ports"]:
+                if macentry["ports"] not in uplinks:
+                    if macentry["ports"] not in l2dict[env][hostname]["interfaces"]:
+                        l2dict[env][hostname]["interfaces"][macentry["ports"]] = {}
+                        l2dict[env][hostname]["interfaces"][macentry["ports"]][macentry["mac"]] = "Vlan" + macentry["vlan"]
+                    elif macentry["ports"] in l2dict[env][hostname]["interfaces"]:
+                        l2dict[env][hostname]["interfaces"][macentry["ports"]][macentry["mac"]] = "Vlan" + macentry["vlan"]
+    
+    conn.disconnect()
+
+# Esta funcion se usa para el manejo de las conexiones paralelas.
+
+def l2data(device,sw_out,user,pas):
+    try:
+        if device in acc["ios"]:
+            conn = ConnectHandler(device_type="cisco_ios_ssh" ,host=device ,username=user ,password=pas)
+            data(conn,device)
+        elif device in acc["nxos"]:
+            conn = ConnectHandler(device_type="cisco_nxos_ssh" ,host=device ,username=user ,password=pas)
+            data(conn,device)
+    except(ConnectionRefusedError, ConnectionResetError):
+        sw_out.append(device)
+        print(f"Error:{device}:ConnectionRefused error")
+        swout_file = open("sw_result.txt","a")
+        swout_file.write(f"Error:{device}:ConnectionRefused error"+"\n")
+        swout_file.close()
+    except(TimeoutError, socket.timeout):
+        sw_out.append(device)
+        print(f"Error:{device}:Timeout error")
+        swout_file = open("sw_result.txt","a")
+        swout_file.write(f"Error:{device}:Timeout error"+"\n")
+        swout_file.close()
+    except(AuthenticationException):
+        sw_out.append(device)
+        print(f"Error:{device}:Authenticacion error")
+        swout_file = open("sw_result.txt","a")
+        swout_file.write(f"Error:{device}:Authenticacion error"+"\n")
+        swout_file.close()
+    except(SSHException, NetmikoTimeoutException):
+        try:
+            conn = ConnectHandler(device_type="cisco_ios_telnet" ,host=device ,username=user ,password=pas)
+            data(conn,device)
+        except(ConnectionRefusedError, ConnectionResetError):
+            sw_out.append(device)
+            print(f"Error:{device}:ConnectionRefused error")
+            swout_file = open("sw_result.txt","a")
+            swout_file.write(f"Error:{device}:ConnectionRefused error"+"\n")
+            swout_file.close()
+        except(TimeoutError, socket.timeout):
+            sw_out.append(device)
+            print(f"Error:{device}:Timeout error")
+            swout_file = open("sw_result.txt","a")
+            swout_file.write(f"Error:{device}:Timeout error"+"\n")
+            swout_file.close()
+        except(AuthenticationException):
+            sw_out.append(device)
+            print(f"Error:{device}:Authenticacion error")
+            swout_file = open("sw_result.txt","a")
+            swout_file.write(f"Error:{device}:Authenticacion error"+"\n")
+            swout_file.close()
+    except(EOFError):
+        sw_out.append(device)
+        print(f"Error:{device}:EOF error")
+        swout_file = open("sw_result.txt","a")
+        swout_file.write(f"Error:{device}:EOF error"+"\n")
+        swout_file.close()
+
+def main():
+
+    sw_out = []
+    total_sw = len(acc["nxos"]) + len(acc["ios"])
+    tiempo1 = datetime.now()
+    tiempo_inicial = tiempo1.strftime("%H:%M:%S")
+    print(f"Hora de inicio: {tiempo_inicial}",f"Total de equipos a validar: {str(total_sw)}",sep="\n")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        ejecucion = {executor.submit(l2data,device,sw_out): device for device in acc["nxos"]}
+    for output in concurrent.futures.as_completed(ejecucion):
+        output.result()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        ejecucion = {executor.submit(l2data,device,sw_out): device for device in acc["ios"]}
+    for output in concurrent.futures.as_completed(ejecucion):
+        output.result()
+
+    # La data se almacena en un JSON.
+
+    file = open("l2.json","w")
+    data = json.dumps(l2dict, indent=4)
+    file.write(data)
+    file.close()
+
+    contador_out = len(sw_out)
+    tiempo2 = datetime.now()
+    tiempo_final = tiempo2.strftime("%H:%M:%S")
+    tiempo_ejecucion = tiempo2 - tiempo1
+    print(f"Hora de finalizacion: {tiempo_final}", f"Tiempo de ejecucion: {tiempo_ejecucion}", f"Total de equipos: {str(total_sw)}",f"Total de equipos fuera: {str(contador_out)}",sep="\n")
+
+if __name__ == "__main__":
+    main()
---
+++ newtorkdb/l3data.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#v1.0.0
+
+import json
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import SSHException, AuthenticationException
+from ntc_templates.parse import parse_output
+
+core = {"nxos":[], "ios":[]}
+l3dict = {"nxos":[], "ios":[]}
+
+# El objetivo de este script es la extraccion de las tablas ARP de los distintos core de la red.
+# Existen dos funciones, una para NXOS y otra para IOS, esto debido a la diferencia en los output de cada OS.
+# La data se almacena en un JSON.
+
+def l3data_nxos(core,l3dict,user,pas):
+    for device in core["nxos"]:
+        conn = ConnectHandler(device_type="cisco_nxos_ssh" ,host=device ,username=user ,password=pas)
+        print(f"Extrayendo data l3 --> {conn.find_prompt()}")
+        arp = parse_output(platform="cisco_nxos",command="show ip arp",data=conn.send_command(f"show ip arp"))
+        for entry in arp:
+            if entry["interface"] not in l3dict["nxos"].keys():
+                l3dict["nxos"][entry["address"]] = {}
+                l3dict["nxos"][entry["address"]]["l3inter"] = entry["interface"]
+                l3dict["nxos"][entry["address"]]["mac"] = entry["mac"]
+            elif entry["interface"] in l3dict["nxos"].keys():
+                pass
+        conn.disconnect()
+
+def l3data_ios(core,l3dict,user,pas):
+    for device in core["ios"]:
+        conn = ConnectHandler(device_type="cisco_ios_ssh" ,host=device ,username=user ,password=pas)
+        print(f"Extrayendo data l3 --> {conn.find_prompt()}")
+        arp = parse_output(platform="cisco_ios",command="show ip arp",data=conn.send_command(f"show ip arp"))
+        for entry in arp:
+            if entry["interface"] not in l3dict["ios"].keys():
+                l3dict["ios"][entry["address"]] = {}
+                l3dict["ios"][entry["address"]]["l3inter"] = entry["interface"]
+                l3dict["ios"][entry["address"]]["mac"] = entry["mac"]
+            elif entry["interface"] in l3dict["ios"].keys():
+                pass
+        conn.disconnect()
+
+def l3data(core,l3dict):
+    try:
+        l3data_nxos(core,l3dict)
+        l3data_ios(core,l3dict)
+    except(ConnectionRefusedError):
+        print(f"Error --> ConnectionRefused error")
+    except(AuthenticationException):
+        print(f"Error --> Authenticacion error")
+    except(SSHException):
+        print(f"Error --> ConnectionRefused error")
+    except(EOFError):
+        print(f"Error --> EOF error")
+
+    file = open("l3.json","w")
+    data = json.dumps(l3dict, indent=4)
+    file.write(data)
+    file.close()
