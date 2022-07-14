5478aab652ba17f6f140e91ac02ee65b93c8dae2
Jorge Marcano <jamg1700@gmail.com>
2022-07-14 06:16:36 -0400

    commented parts that are being rebuilt

--- cisco_support_api/cisco_api_report.py
+++ cisco_support_api/cisco_api_report.py
@@ -2,12 +2,12 @@
 # v1.0.4
 
 import concurrent.futures, pandas as pd, os
-from bugapi import bugdata, productnames
+# from bugapi import bugdata, productnames
 from datetime import datetime
 from devicedata import device_data, devices, offline, ios, nxos
 from getpass import getuser
 from supportapi import supportdata, header, supportdict
-from psirtapi import psirtdata, header, os_dict
+# from psirtapi import psirtdata, header, os_dict
 from sqlalchemy import create_engine
 from tqdm import tqdm
 
@@ -49,26 +49,26 @@ serialdf.to_sql('serial', con=engine ,index=False ,if_exists="replace")
 productdf = pd.DataFrame(supportdict["productdata"])
 productdf.to_sql('product', con=engine ,index=False ,if_exists="replace")
 
-bugdata(header,productnames)
-psirtdata(header,os_dict)
+# bugdata(header,productnames)
+# psirtdata(header,os_dict)
 
-df1 = read_csv("supportdata.csv", encoding='latin1', on_bad_lines="skip")
-df2 = read_csv("bugdata.csv", encoding='latin1', on_bad_lines="skip")
-df3 = read_csv("psirtdata.csv", encoding='latin1', on_bad_lines="skip")
-writer = ExcelWriter("cisco_api_report.xlsx", engine='xlsxwriter')
-df1.to_excel(writer,sheet_name="support_data",index=None,header=True,freeze_panes=(1,0))
-df2.to_excel(writer,sheet_name="bug_data",index=None,header=True,freeze_panes=(1,0))
-df3.to_excel(writer,sheet_name="cve_data",index=None,header=True,freeze_panes=(1,0))
-writer.save()
+# df1 = read_csv("supportdata.csv", encoding='latin1', on_bad_lines="skip")
+# df2 = read_csv("bugdata.csv", encoding='latin1', on_bad_lines="skip")
+# df3 = read_csv("psirtdata.csv", encoding='latin1', on_bad_lines="skip")
+# writer = ExcelWriter("cisco_api_report.xlsx", engine='xlsxwriter')
+# df1.to_excel(writer,sheet_name="support_data",index=None,header=True,freeze_panes=(1,0))
+# df2.to_excel(writer,sheet_name="bug_data",index=None,header=True,freeze_panes=(1,0))
+# df3.to_excel(writer,sheet_name="cve_data",index=None,header=True,freeze_panes=(1,0))
+# writer.save()
 
-os.remove("supportdata.csv")
-os.remove("bugdata.csv")
-os.remove("psirtdata.csv")
+# os.remove("supportdata.csv")
+# os.remove("bugdata.csv")
+# os.remove("psirtdata.csv")
 
-contador_out = len(sw_out)
-tiempo2 = datetime.now()
-tiempo_final = tiempo2.strftime("%H:%M:%S")
-tiempo_ejecucion = tiempo2 - tiempo1
-total_off = len(offline)
-print(f"Hora de finalizacion: {tiempo_final}", f"Tiempo de ejecucion: {tiempo_ejecucion}", f"Total de equipos validados: {str(total_sw)}",
-f"Total de equipos fuera: {str(contador_out)}",sep="\n")
+# contador_out = len(sw_out)
+# tiempo2 = datetime.now()
+# tiempo_final = tiempo2.strftime("%H:%M:%S")
+# tiempo_ejecucion = tiempo2 - tiempo1
+# total_off = len(offline)
+# print(f"Hora de finalizacion: {tiempo_final}", f"Tiempo de ejecucion: {tiempo_ejecucion}", f"Total de equipos validados: {str(total_sw)}",
+# f"Total de equipos fuera: {str(contador_out)}",sep="\n")
