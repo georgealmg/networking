f2f70ab932a4d99e8629372dac77c6b9373a0ae4
Jorge Marcano <jamg1700@gmail.com>
2022-07-14 06:52:52 -0400

    added bug api

--- cisco_support_api/cisco_api_report.py
+++ cisco_support_api/cisco_api_report.py
@@ -2,9 +2,9 @@
 # v1.0.4
 
 import concurrent.futures, pandas as pd, os
-# from bugapi import bugdata, productnames
+from bugapi import bugdata, Bdata, productnames
 from datetime import datetime
-from devicedata import device_data, devices, offline, ios, nxos
+from devicedata import device_data, Ddata, devices, offline, ios, nxos
 from getpass import getuser
 from supportapi import supportdata, header, supportdict
 # from psirtapi import psirtdata, header, os_dict
@@ -19,7 +19,7 @@ except(FileNotFoundError):
 
 engine = create_engine("mysql+pymysql://root:pr0gr4m_SQL@172.25.192.1/ciscoapi")
 
-total = len(devices.keys())
+total = len(devices)
 tiempo1 = datetime.now()
 tiempo_inicial = tiempo1.strftime("%H:%M:%S")
 print(f"Hora de inicio: {tiempo_inicial}")
@@ -28,7 +28,7 @@ def main():
 
     with tqdm(total=len(total), desc="Extracting device data") as pbar:
         with concurrent.futures.ThreadPoolExecutor(max_workers=40) as executor_ios:
-            ejecucion_ios = {executor_ios.submit(device_data,sw,offline,ios,nxos): sw for sw in devices.keys()}
+            ejecucion_ios = {executor_ios.submit(device_data,sw,offline,ios,nxos): sw for sw in devices}
         for output_ios in concurrent.futures.as_completed(ejecucion_ios):
             output_ios.result()
             pbar.update(1)
@@ -36,7 +36,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-devicesdf = pd.DataFrame(devices)
+devicesdf = pd.DataFrame(Ddata)
 devicesdf.to_sql('devices', con=engine ,index=False ,if_exists="replace")
 
 supportdata(devices,header,supportdict)
@@ -49,20 +49,17 @@ serialdf.to_sql('serial', con=engine ,index=False ,if_exists="replace")
 productdf = pd.DataFrame(supportdict["productdata"])
 productdf.to_sql('product', con=engine ,index=False ,if_exists="replace")
 
-# bugdata(header,productnames)
+bugdata(header,productnames,devicesdf,productdf,Bdata)
+bugdf = pd.DataFrame(Bdata)
+bugdf.to_sql('bugs', con=engine ,index=False ,if_exists="replace")
+
 # psirtdata(header,os_dict)
 
-# df1 = read_csv("supportdata.csv", encoding='latin1', on_bad_lines="skip")
-# df2 = read_csv("bugdata.csv", encoding='latin1', on_bad_lines="skip")
 # df3 = read_csv("psirtdata.csv", encoding='latin1', on_bad_lines="skip")
 # writer = ExcelWriter("cisco_api_report.xlsx", engine='xlsxwriter')
-# df1.to_excel(writer,sheet_name="support_data",index=None,header=True,freeze_panes=(1,0))
-# df2.to_excel(writer,sheet_name="bug_data",index=None,header=True,freeze_panes=(1,0))
 # df3.to_excel(writer,sheet_name="cve_data",index=None,header=True,freeze_panes=(1,0))
 # writer.save()
 
-# os.remove("supportdata.csv")
-# os.remove("bugdata.csv")
 # os.remove("psirtdata.csv")
 
 # contador_out = len(sw_out)
