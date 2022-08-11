b36577d0fcc7bbe9c65fc632099d8fdde5a966ee
Jorge Marcano <jmarcano@entel.cl>
2022-08-10 21:44:06 -0400

    addapted the statements to the new columns

--- cisco_support_api/altertables.sql
+++ cisco_support_api/altertables.sql
@@ -2,8 +2,8 @@ use ciscoapi;
 
 alter table devices modify column IP varchar(15);
 alter table devices modify column SerialNumber varchar(20);
-alter table devices modify column osFamily varchar(5);
-alter table devices modify column osVersion varchar(10);
+alter table devices modify column OSFamily varchar(5);
+alter table devices modify column OSversion varchar(10);
 
 alter table eox modify column EndOfSaleDate date;
 alter table eox modify column LastDateOfSupport date;
@@ -13,29 +13,27 @@ alter table products modify column ProductReleaseDate date;
 alter table software modify column SoftwareReleaseDate date;
 
 CREATE TABLE IF NOT EXISTS bugs (
-    product_id TEXT,
-    os_version TEXT,
-    bug_id TEXT,
+    ProductID TEXT,
+    OSversion TEXT,
+    BugID TEXT,
     headline MEDIUMTEXT,
     severity INT,
-    bug_status VARCHAR(9),
-    last_modified_date DATE,
-    known_fixed_releases TEXT
+    status VARCHAR(9),
+    LastModifiedDate DATE,
+    KnownFixedReleases TEXT
 );
 
 CREATE TABLE IF NOT EXISTS psirt (
-    os_familiy VARCHAR(5),
-    os_version TEXT,
-    bug_id TEXT,
+    OSfamily VARCHAR(5),
+    OSversion TEXT,
+    BugID TEXT,
     advisoryTitle MEDIUMTEXT,
     cves MEDIUMTEXT,
     cwe TEXT,
-    cve_version FLOAT(0),
-    cve_status VARCHAR(9),
+    status VARCHAR(9),
     firstPublished DATE,
     lastUpdated DATE,
     severity VARCHAR(9),
-    affected_release TEXT,
     first_fixed TEXT,
     url MEDIUMTEXT
 );
