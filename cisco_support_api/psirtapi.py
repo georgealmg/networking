d6c5cc4b5c348d06faa81c2bd74a179dee255feb
Jorge Marcano <jamg1700@gmail.com>
2022-08-11 19:18:38 -0400

    added concurrent.futures

--- cisco_support_api/psirtapi.py
+++ cisco_support_api/psirtapi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#v1.0.4
+#v1.0.5
 
-import requests
+import concurrent.futures, requests
 from ratelimit import RateLimitException, limits
 from backoff import on_exception, expo
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -19,7 +19,35 @@ OSdata = []
 
 @on_exception(expo,RateLimitException,max_tries=5)
 @limits(calls=10,period=1)
-def psirtdata(devicesdf,header,osdict,OSdata):
+def apicall(os,header,osdict,OSdata,pbar):
+
+    for version in osdict[os]:
+        url = f"https://api.cisco.com/security/advisories/{os}?version={version}"
+        response = requests.get(url, headers=header)
+        if response.status_code == 200:
+            psirt = response.json()
+            if "errorCode" not in psirt.keys():
+                psirt = response.json()["advisories"]
+                for entry in psirt:
+                    OSdata.append({"OSfamily":os,"OSversion":version,"BugID":str(entry["bugIDs"]),"advisoryTitle":entry["advisoryTitle"],"cves":str(entry["cves"])
+                    ,"cwe":str(entry["cwe"]),"status":entry["status"],"firstPublished":entry["firstPublished"],
+                    "lastUpdated":entry["lastUpdated"],"severity":entry["sir"],"firstFixed":str(entry["firstFixed"])
+                    ,"url":entry["publicationUrl"]})
+            elif "errorCode" in psirt.keys():
+                    OSdata.append({"OSfamily":os,"OSversion":version,"BugID":"N/A","advisoryTitle":"N/A","cves":"N/A"
+                    ,"cwe":"N/A","status":"N/A","firstPublished":"N/A",
+                    "lastUpdated":"N/A","severity":"N/A","firstFixed":"N/A"
+                    ,"url":"N/A"})
+        elif response.status_code != 200:
+            errorMessage = "HTTPError:"+str(response.status_code)
+            OSdata.append({"OSfamily":os,"OSversion":version,"BugID":errorMessage,"advisoryTitle":errorMessage,"cves":errorMessage
+            ,"cwe":errorMessage,"status":errorMessage,"firstPublished":errorMessage,
+            "lastUpdated":errorMessage,"severity":errorMessage,"firstFixed":errorMessage
+            ,"url":errorMessage})
+    
+        pbar.update(1)
+
+def psirtdata(devicesdf,osdict,OSdata):
 
     for entry in devicesdf.values:
         try:
@@ -42,30 +70,9 @@ def psirtdata(devicesdf,header,osdict,OSdata):
     total = 0
     for os in osdict.keys():
         total = total + len(osdict[os])
+
     with tqdm(total=total, desc="Extracting psirt data") as pbar:
-        for os in osdict.keys(): 
-            for version in osdict[os]:
-                url = f"https://api.cisco.com/security/advisories/{os}?version={version}"
-                response = requests.get(url, headers=header)
-                if response.status_code == 200:
-                    psirt = response.json()
-                    if "errorCode" not in psirt.keys():
-                        psirt = response.json()["advisories"]
-                        for entry in psirt:
-                            OSdata.append({"OSfamily":os,"OSversion":version,"BugID":str(entry["bugIDs"]),"advisoryTitle":entry["advisoryTitle"],"cves":str(entry["cves"])
-                            ,"cwe":str(entry["cwe"]),"status":entry["status"],"firstPublished":entry["firstPublished"],
-                            "lastUpdated":entry["lastUpdated"],"severity":entry["sir"],"firstFixed":str(entry["firstFixed"])
-                            ,"url":entry["publicationUrl"]})
-                    elif "errorCode" in psirt.keys():
-                            OSdata.append({"OSfamily":os,"OSversion":version,"BugID":"N/A","advisoryTitle":"N/A","cves":"N/A"
-                            ,"cwe":"N/A","status":"N/A","firstPublished":"N/A",
-                            "lastUpdated":"N/A","severity":"N/A","firstFixed":"N/A"
-                            ,"url":"N/A"})
-                elif response.status_code != 200:
-                    errorMessage = "HTTPError:"+str(response.status_code)
-                    OSdata.append({"OSfamily":os,"OSversion":version,"BugID":errorMessage,"advisoryTitle":errorMessage,"cves":errorMessage
-                    ,"cwe":errorMessage,"status":errorMessage,"firstPublished":errorMessage,
-                    "lastUpdated":errorMessage,"severity":errorMessage,"firstFixed":errorMessage
-                    ,"url":errorMessage})
-        
-                pbar.update(1)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            ejecucion = {executor.submit(apicall,os,header,osdict,OSdata,pbar): os for os in osdict.keys()}
+        for output in concurrent.futures.as_completed(ejecucion):
+            output.result()
