ce6b5aa1c1603cedd7452c4d61a7d44d34230c85
Jorge Marcano <jmarcano@entel.cl>
2022-09-20 20:20:16 -0300

    replaced df to excel with df to sql

--- networkdb/networkdb.py
+++ networkdb/networkdb.py
@@ -1,10 +1,11 @@
 # !/usr/bin/env python3
-# v1.0.0
+# v1.0.1
 
-import os, pandas as pd, re
-from acidata import apics, headers, nodes, acidata
+import os, pandas as pd, sqlalchemy as db, re
 from dotenv import load_dotenv
 from getpass import getuser
+from netifaces import gateways
+from acidata import apics, headers, nodes, acidata
 from l3data import core, l3data
 from l2data import devices, l2data
 from dnsdata import recordstoignore, dnsdict, dnsdata
@@ -21,25 +22,39 @@ user = os.environ["user"]
 pas = os.environ["netpass"]
 dnsuser = os.environ["dnsuser"]
 dnspas = os.environ["dnspass"]
+gateway = gateways()["default"][2][0]
+engine = db.create_engine(f"mysql+pymysql://root:pr0gr4m@{gateway}/networkdb")
+conn = engine.connect()
+out = []
 
 # The ARP data, MAC and DNS (A records) will be stored in individual dataframes.
+# To make sure that the data will be properly merged new columns (joinkey) will be created based on the MAC and VLAN columns.
 
 l3df = []
-l3data(user,pas,core,l3df)
+l3data(user,pas,core,l3df,out)
 l3df = pd.DataFrame(l3df)
 l3df = l3df[l3df["mac"].str.contains("Incomplete")==False]
 l3df = l3df[l3df["mac"].str.contains("INCOMPLETE")==False]
+l3df["joinkey"] = l3df["mac"]+l3df["vlan"]
+l3df.to_sql("l3",conn,"networkdb",if_exists="append",index=False)
 
 l2df =[]
-l2data(user,pas,devices,l2df)
+l2data(user,pas,devices,l2df,out)
 l2df = pd.DataFrame(l2df)
 l2df["sw"] = l2df["sw"].replace(regex={r"#":""})
+l2df["joinkey"] = l2df["mac"]+l2df["vlan"]
+l2df.to_sql("l2",conn,"networkdb",if_exists="append",index=False)
+
+# This csv will store data about the failed connection attempts.
+outdf = pd.DataFrame(out)
+outdf.to_csv("offline.csv",sep=",",index=False)
 
 dnsdata(dnsuser,dnspas,recordstoignore,dnsdict)
 dnsdf = []
 for entry in dnsdict.items():
-    dnsdf.append({"ip":entry[0],"dns":entry[1]})
+    dnsdf.append({"ip":entry[0],"dns":str(entry[1])})
 dnsdf = pd.DataFrame(dnsdf)
+dnsdf.to_sql("dns",conn,"networkdb",if_exists="append",index=False)
 
 # The ACI data will be stored in its own dataframe, the data coming from ACI will be processed to fit the requeriments needed 
 # for the merging of data.
@@ -55,12 +70,10 @@ for sw in acidf["sw"]:
     for id in range(len(ids)):
         leaf = re.sub(ids[id],nodes[ids[id]],ids[id])
         lfs.append(leaf)
-    leafs.append(lfs)
+    leafs.append(str(lfs))
 acidf["sw"] = leafs
-
 acidf["sw"] = acidf["sw"].replace(regex={r"\[":"",r"\]":"","', '":"/"})
 acidf["interface"] = acidf["interface"].replace(regex={r"\[":"",r"\]":"","eth":"Eth"})
-
 acidf["mac"] = acidf["mac"].str.lower()
 for mac in acidf["mac"]:
     dotmac = mac.split(":")
@@ -68,59 +81,5 @@ for mac in acidf["mac"]:
     dotmacs.append(mac)
 acidf["mac"] = dotmacs
 acidf["vlan"] = acidf["vlan"].replace(regex={r"vlan-":"Vlan"})
-
-acidf_c = acidf.copy()
-acidf_c["dns"] = "--"
-acidf_c = acidf_c[["ip","mac","vlan","dns","sw","interface"]]
-
-# To make sure that the data will be properly merged new columns will be created based on the MAC and VLAN columns.
-
-acidf["join"] = acidf["mac"]+acidf["vlan"]
-l2df["join"] = l2df["mac"]+l2df["vlan"]
-l3df["join"] = l3df["mac"]+l3df["vlan"]
-
-# The legacydf stores data about the devices connected in legacy networks (non-ACI),
-# since the inner joins will drop data about IPs that don't have a DNS A record a copy of the dataframe is created,
-# said copy will have a DNS column with non-relevant data to make sure the concatenation goes well.
-
-legacydf = l3df.merge(l2df,how="inner",on="join")
-l3l2df = legacydf.copy()
-l3l2df["dns"] = "--"
-legacydf = legacydf.merge(dnsdf,how="inner",left_on="ip_x",right_on="ip")
-legacydf = legacydf.drop(columns=["ip_x","vlan_x","mac_x","join","ip_y"])
-legacydf = legacydf.rename(columns={"mac_y":"mac","vlan_y":"vlan"})
-legacydf = legacydf[["ip","mac","vlan","dns","sw","interface"]]
-l3l2df = l3l2df.drop(columns=["vlan_x","mac_x","join","ip_y"])
-l3l2df = l3l2df.rename(columns={"mac_y":"mac","vlan_y":"vlan","ip_x":"ip"})
-l3l2df = l3l2df[["ip","mac","vlan","dns","sw","interface"]]
-legacydf = pd.concat([legacydf,l3l2df])
-legacydf = legacydf.drop_duplicates(subset=["ip"], keep="first")
-
-# The newdf stores data about the devices connected in ACI networks,
-# since the inner joins will drop data about IPs that don't have a DNS A record a copy of the dataframe is created,
-# said copy will have a DNS column with non-relevant data to make sure the concatenation goes well.
-
-newdf = acidf.merge(l3df,how="inner",on="join")
-newdf_c = newdf.copy()
-newdf_c["dns"] = "--"
-newdf_c = newdf_c.drop(columns=["vlan_x","mac_x","join","ip_y"])
-newdf_c = newdf_c.rename(columns={"mac_y":"mac","vlan_y":"vlan","ip_x":"ip"})
-newdf_c = newdf_c[["ip","mac","vlan","dns","sw","interface"]]
-newdf = newdf.merge(dnsdf,how="inner",left_on="ip_y",right_on="ip")
-newdf = newdf.rename(columns={"mac_y":"mac","vlan_y":"vlan"})
-newdf = newdf[["ip","mac","vlan","dns","sw","interface"]]
-newdf = pd.concat([newdf,newdf_c])
-acidf = acidf.drop(columns=["join"])
-acidf = acidf.merge(dnsdf,how="inner",on="ip")
-acidf = acidf[["ip","mac","vlan","dns","sw","interface"]]
-acidf = pd.concat([acidf,acidf_c])
-acidf = acidf.drop_duplicates(subset=["ip"], keep="first")
-newdf = pd.concat([acidf,newdf])
-newdf = newdf.drop_duplicates(subset=["ip"], keep="first")
-
-# The end result is a Excel file that has the logical location of all endpoints in the DC network.
-
-writer = pd.ExcelWriter("networkdb.xlsx", engine='xlsxwriter')
-newdf.to_excel(writer,sheet_name="ACI",index=None,header=True,freeze_panes=(1,0))
-writer.save()
-print("Done")
+acidf["joinkey"] = acidf["mac"]+acidf["vlan"]
+acidf.to_sql("aci",conn,"networkdb",if_exists="append",index=False)
