d48aa7481e1310d587393ef2f6f75750d77c7623
Jorge Marcano <jmarcano@entel.cl>
2022-10-13 16:32:21 -0300

    inprovements to reduce the number of api calls

--- cisco_support_api/supportapi.py
+++ cisco_support_api/supportapi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#v1.0.5
+#v1.0.6
 
-import requests
+import requests, numpy as np
 from ratelimit import RateLimitException, limits
 from backoff import on_exception, expo
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -9,34 +9,57 @@ from tqdm import tqdm
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 Edata,SFdata,Sdata,Pdata = [],[],[],[]
-productsid =  []
 supportdict = {}
 
 @on_exception(expo,RateLimitException,max_tries=5)
 @limits(calls=5,period=1)
-def eoxdata(headers,serialnumbers,Edata):
+def serialdata(headers,serialnumbers,Sdata):
 
-    with tqdm(total=len(serialnumbers), desc="Extracting eox data") as pbar:
-        for id in serialnumbers:
-            url = f"https://api.cisco.com/supporttools/eox/rest/5/EOXBySerialNumber/1/{id}?responseencoding=json"
+    serial_array = np.array_split(serialnumbers,(len(serialnumbers)//50)+1)
+    with tqdm(total=len(serial_array), desc="Extracting serial data") as pbar:
+        for entry in serial_array:
+            number = str(entry).replace("'","").replace(" ",",").replace("["," ").replace("]","").replace("\n","").replace(" ","")
+            url = f"https://api.cisco.com/sn2info/v2/coverage/summary/serial_numbers/{number}"
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
-                eox = response.json()["EOXRecord"][0]
-                if "EOXError" in eox.keys():
-                    if "does not exist" in eox["EOXError"]["ErrorDescription"]:
-                        product =  eox["EOXError"]["ErrorDataValue"]
-                        productsid.append(product)
-                        EndOfSaleDate = "N/A"
-                        LastDateOfSupport = "N/A"
-                        EOXMigrationDetails = "N/A"
-                elif "EOXError" not in eox.keys():
-                    product =  eox["EOLProductID"]
-                    productsid.append(product)
-                    EndOfSaleDate =  eox["EndOfSaleDate"]["value"]
-                    LastDateOfSupport = eox["LastDateOfSupport"]["value"]
-                    EOXMigrationDetails = eox["EOXMigrationDetails"]["MigrationProductId"]
-                    pass
-                Edata.append({"ProductID":id,"EndOfSaleDate":EndOfSaleDate,"LastDateOfSupport":LastDateOfSupport,
+                for record in response.json()["serial_numbers"]:
+                    serial = record["sr_no"]
+                    customer = record["contract_site_customer_name"]
+                    contractEndDate = record["covered_product_line_end_date"]
+                    isCovered = record["is_covered"]
+                    Sdata.append({"SerialNumber":serial,"Customer":customer,
+                    "ContractEndDate":contractEndDate,"IsCovered":isCovered})
+            elif response.status_code != 200:
+                errorMessage = "HTTPError:"+str(response.status_code)
+                Sdata.append({"SerialNumber":number,"Customer":errorMessage,
+                "ContractEndDate":errorMessage,"IsCovered":errorMessage})
+            pbar.update(1)
+
+@on_exception(expo,RateLimitException,max_tries=5)
+@limits(calls=5,period=1)
+def eoxdata(headers,productsid,Edata):
+
+    product_array = np.array(productsid)
+    product_array = np.array_split(product_array,(len(productsid)//20)+1)
+    with tqdm(total=len(product_array), desc="Extracting eox data") as pbar:
+        for entry in product_array:
+            id = str(entry).replace("'","").replace(" ",",").replace("["," ").replace("]","").replace("\n","").replace(" ","")
+            url = f"https://api.cisco.com/supporttools/eox/rest/5/EOXByProductID/1/{id}"
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                for record in response.json()["EOXRecord"]:
+                    if "EOXError" in record.keys():
+                        if "does not exist" in record["EOXError"]["ErrorDescription"]:
+                            product = record["EOXInputValue"]
+                            EndOfSaleDate = "N/A"
+                            LastDateOfSupport = "N/A"
+                            EOXMigrationDetails = "N/A"
+                    elif "EOXError" not in record.keys():
+                        product = record["EOXInputValue"]
+                        EndOfSaleDate =  record["EndOfSaleDate"]["value"]
+                        LastDateOfSupport = record["LastDateOfSupport"]["value"]
+                        EOXMigrationDetails = record["EOXMigrationDetails"]["MigrationProductId"]
+                Edata.append({"ProductID":product,"EndOfSaleDate":EndOfSaleDate,"LastDateOfSupport":LastDateOfSupport,
                 "EOXMigrationDetails":EOXMigrationDetails})
             elif response.status_code != 200:
                 errorMessage = "HTTPError:"+str(response.status_code)
@@ -45,44 +68,22 @@ def eoxdata(headers,serialnumbers,Edata):
             pbar.update(1)
 
 @on_exception(expo,RateLimitException,max_tries=5)
-@limits(calls=5,period=1)
-def serialdata(headers,serialnumbers,Sdata):
-
-    with tqdm(total=len(serialnumbers), desc="Extracting serial data") as pbar:
-        for number in serialnumbers:
-            url = f"https://api.cisco.com/sn2info/v2/coverage/summary/serial_numbers/{number}" 
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                try:
-                    serial = response.json()["serial_numbers"][0]
-                    customer = serial["contract_site_customer_name"]
-                    contractEndDate = serial["covered_product_line_end_date"]
-                    isCovered = serial["is_covered"]
-                except(KeyError):
-                    customer = "N/A"
-                    contractEndDate = "N/A"
-                    isCovered = "N/A"
-                Sdata.append({"SerialNumber":number,"Customer":customer,
-                "ContractEndDate":contractEndDate,"IsCovered":isCovered})
-            elif response.status_code != 200:
-                errorMessage = "HTTPError:"+str(response.status_code)
-                Sdata.append({"SerialNumber":number,"Customer":errorMessage,
-                "ContractEndDate":errorMessage,"IsCovered":errorMessage})
-            pbar.update(1)
-
-@on_exception(expo,RateLimitException,max_tries=5)
 @limits(calls=10,period=1)
 def productdata(headers,productsid,Pdata):
 
-    with tqdm(total=len(productsid), desc="Extracting product data") as pbar:
-        for id in productsid:
+    product_array = np.array(productsid)
+    product_array = np.array_split(product_array,(len(productsid)//5)+1)
+    with tqdm(total=len(product_array), desc="Extracting product data") as pbar:
+        for entry in product_array:
+            id = str(entry).replace("'","").replace(" ",",").replace("["," ").replace("]","").replace("\n","").replace(" ","")
             url = f"https://api.cisco.com/product/v1/information/product_ids/{id}"
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
-                product = response.json()["product_list"][0]
-                ProductReleaseDate = product["release_date"]
-                ProductSeries = product["product_series"].replace(" ","%20")
-                Pdata.append({"ProductID":id,"ProductReleaseDate":ProductReleaseDate,"ProductSeries":ProductSeries})
+                for record in response.json()["product_list"]:
+                    product = record["product_id"]
+                    ProductReleaseDate = record["release_date"]
+                    ProductSeries = record["product_series"].replace(" ","%20")
+                    Pdata.append({"ProductID":product,"ProductReleaseDate":ProductReleaseDate,"ProductSeries":ProductSeries})
             elif response.status_code != 200:
                 errorMessage = "HTTPError:"+str(response.status_code)
                 Pdata.append({"ProductID":id,"ProductReleaseDate":errorMessage,"ProductSeries":errorMessage})
@@ -92,29 +93,37 @@ def productdata(headers,productsid,Pdata):
 @limits(calls=10,period=1)
 def softwaredata(headers,productsid,SFdata):
 
-    with tqdm(total=len(productsid), desc="Extracting software data") as pbar:
-        for id in productsid:
+    product_array = np.array(productsid)
+    product_array = np.array_split(product_array,(len(productsid)//10)+1)
+    with tqdm(total=len(product_array), desc="Extracting software data") as pbar:
+        for entry in product_array:
+            print(entry)
+            id = str(entry).replace("'","").replace(" ",",").replace("["," ").replace("]","").replace("\n","").replace(" ","")
+            print(id)
             url = f"https://api.cisco.com/software/suggestion/v2/suggestions/software/productIds/{id}"
+            print(url)
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 software = response.json()["productList"]
-                for entry in software:
+                for record in software:
+                    product = record["product"]["basePID"]
                     try:
-                        if entry["suggestions"][0]["isSuggested"] == "Y":
-                            RosVersion = entry["suggestions"][0]["releaseFormat1"]
-                            SreleaseDate = entry["suggestions"][0]["releaseDate"]
-                            imageName = entry["suggestions"][0]["images"][0]["imageName"]
-                        elif entry["suggestions"][0]["isSuggested"] == "N":
-                            RosVersion = "Validate"
-                            SreleaseDate = "Validate"
-                            imageName = "Validate"
-                        SFdata.append({"ProductID":id,"RecommendedOSversion":RosVersion,"SoftwareReleaseDate":SreleaseDate,
-                        "ImageName":imageName})
+                        for subrecord in record["suggestions"]:
+                            if subrecord["isSuggested"] == "Y":
+                                RosVersion = subrecord["releaseFormat1"]
+                                SreleaseDate = subrecord["releaseDate"]
+                                imageName = subrecord["images"][0]["imageName"]
+                            elif subrecord["isSuggested"] == "N":
+                                RosVersion = "Validate"
+                                SreleaseDate = "Validate"
+                                imageName = "Validate"
+                            SFdata.append({"ProductID":product,"RecommendedOSversion":RosVersion,"SoftwareReleaseDate":SreleaseDate,
+                            "ImageName":imageName})
                     except(KeyError,UnboundLocalError):
                         RosVersion = "Validate"
                         SreleaseDate = "Validate"
                         imageName = "Validate"
-                        SFdata.append({"ProductID":id,"RecommendedOSversion":RosVersion,"SoftwareReleaseDate":SreleaseDate,
+                        SFdata.append({"ProductID":product,"RecommendedOSversion":RosVersion,"SoftwareReleaseDate":SreleaseDate,
                         "ImageName":imageName})
             elif response.status_code != 200:
                 errorMessage = "HTTPError:"+str(response.status_code)
@@ -133,10 +142,11 @@ def supportdata(env_vars,devicesdf,supportdict):
     token = response.json()["token_type"] + " " + response.json()["access_token"]
     headers = {"Authorization": token}
 
-    serialnumbers = list(devicesdf["SerialNumber"].unique())
+    serialnumbers = devicesdf["SerialNumber"].values
+    productsid = list(devicesdf["ProductID"].unique)
 
-    eoxdata(headers,serialnumbers,Edata)
-    serialdata(headers,productsid,Sdata)
+    serialdata(headers,serialnumbers,Sdata)
+    eoxdata(headers,productsid,Edata)
     softwaredata(headers,productsid,SFdata)
     productdata(headers,productsid,Pdata)
     
