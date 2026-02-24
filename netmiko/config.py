0be37ff8bc075dfc78f1e1ba6b5db263a24fe6ca
Jorge Marcano <71988888+georgealmg@users.noreply.github.com>
2026-02-24 14:36:25 -0300

    Mejoras en script

--- netmiko/config.py
+++ netmiko/config.py
@@ -1,106 +1,108 @@
 #!/usr/bin/env python3
-#v1.0.2
 
-import concurrent.futures, os, socket
-from getpass import getpass, getuser
+import csv, concurrent.futures, os, socket, sys
+from dotenv import load_dotenv
+from getpass import getuser
 from datetime import datetime
 from netmiko import ConnectHandler
-from netmiko.exceptions import SSHException, AuthenticationException
+from netmiko.exceptions import SSHException, AuthenticationException, NetmikoTimeoutException, ReadException
 from tqdm import tqdm
 
 try:
-    os.chdir(f"/mnt/c/Users/{getuser()}/Documents")
+    os.chdir(f"C:/Users/{getuser()}/Documents")
 except(FileNotFoundError):
     os.chdir(os.getcwd())
 
-user = input("Username: ")
-pas = getpass()
-commands = []
-devices,offline,configured = [],[],[]
-result = open("result.txt","w")
-output = open("output.txt","w")
-result.close()
-output.close()
-
-total = len(devices)
-tiempo1 = datetime.now()
-tiempo_inicial = tiempo1.strftime("%H:%M:%S")
-print(f"Hora de inicio: {tiempo_inicial}",f"Total de equipos a configurar: {str(total)}",sep="\n")
-
+load_dotenv("credentials.env")
+user = os.environ["user"]
+password = os.environ["sdn_pass"]
+configc,devices,offline,rows = [],[],[],[]
 for ip in open("IP_validacion.txt","r"):
     devices.append(ip.strip("\n"))
+outputFile = open("output_config.txt","w")
+outputFile.close()
+swout = open("result.csv","w", newline='')
+first_row = ["IP","Comment"]
+writer = csv.DictWriter(swout, fieldnames=first_row)
+writer.writeheader()
 
-def config(sw,conn,configured,commands):
-    hostname = conn.find_prompt()
-    output = conn.send_config_set(commands)
-    output = open("output.txt","a")
-    output.write(output+"\n")
-    output.close()
+choice = input("Por favor indicar si los dispositivos cuentan con IOS o NXOS: ")
+choice = choice.lower()
+quantity = input("Indicar cantidad de comandos: ")
+commands = []
+print("Introducir comando, uno por linea")
+for x in range (0,int(quantity)):
+    command = input()
+    commands.append(command)
+
+def config(device,conn,configc):
+    output = conn.send_config_set(commands, read_timeout=120, terminator=conn.find_prompt())
+    output_file = open("output_config.txt","a")
+    output_file.write(output+"\n")
+    output_file.close()
     conn.save_config()
-    configured.append(sw)
-    result = open("result.txt","a")
-    result.write(f"OK:{sw}:Configurado"+"\n")
-    result.close()
+    configc.append(device)
+    if "Invalid" not in output:
+        rows.append({"IP":device,"Comment":"Configured"})
+    elif "Invalid" in output:
+        rows.append({"IP":device,"Comment":"ConfigError"})
     conn.disconnect()
 
-def connection(sw):
+def connection(device,pbar):
     try:
-        conn = ConnectHandler(device_type= "cisco_ios_ssh",host= sw,username= user,password= pas)
-        config(sw,conn,configured,commands)
+        if choice ==  "ios":
+            conn = ConnectHandler(device_type="cisco_ios_ssh", host=device, username=user, password=password, fast_cli=False,
+            read_timeout_override=180)
+        elif choice == "nxos":
+            conn = ConnectHandler(device_type= "cisco_nxos_ssh", host= device, username= user, password=password, fast_cli=False,
+            read_timeout_override=180)
+        else:
+            sys.exit("Favor indicar sistema operativo de los equipos")
+        config(device,conn,configc)
     except(ConnectionRefusedError, ConnectionResetError):
-        offline.append(sw)
-        result = open("result.txt","a")
-        result.write(f"Error:{sw}:ConnectionRefused error"+"\n")
-        result.close()
-    except(TimeoutError, socket.timeout):
-        offline.append(sw)
-        result = open("result.txt","a")
-        result.write(f"Error:{sw}:Timeout error"+"\n")
-        result.close()
+        rows.append({"IP":device,"Comment":"ConnectionRefused"})
     except(AuthenticationException):
-        offline.append(sw)
-        result = open("result.txt","a")
-        result.write(f"Error:{sw}:Authenticacion error"+"\n")
-        result.close()
-    except(SSHException):
+        rows.append({"IP":device,"Comment":"Authentication"})
+    except(TimeoutError, socket.timeout):
+        rows.append({"IP":device,"Comment":"Timeout"})
+    except(SSHException, NetmikoTimeoutException):
         try:
-            conn = ConnectHandler(device_type= "cisco_ios_telnet",host= sw,username= user,password= pas)
-            config(sw,conn,configured,commands)
+            conn = ConnectHandler(device_type= "cisco_ios_telnet", host=device, username=user, password=password, fast_cli=False,
+            read_timeout_override=180)
+            config(device,conn,configc)
         except(ConnectionRefusedError, ConnectionResetError):
-            offline.append(sw)
-            result = open("result.txt","a")
-            result.write(f"Error:{sw}:ConnectionRefused error"+"\n")
-            result.close()
-        except(TimeoutError, socket.timeout):
-            offline.append(sw)
-            result = open("result.txt","a")
-            result.write(f"Error:{sw}:Timeout error"+"\n")
-            result.close()
+            rows.append({"IP":device,"Comment":"ConnectionRefused"})
         except(AuthenticationException):
-            offline.append(sw)
-            result = open("result.txt","a")
-            result.write(f"Error:{sw}:Authenticacion error"+"\n")
-            result.close()
+            rows.append({"IP":device,"Comment":"Authentication"})
+        except(TimeoutError, socket.timeout, NetmikoTimeoutException):
+            rows.append({"IP":device,"Comment":"Timeout"})
+        except(ReadException):
+            rows.append({"IP":device,"Comment":"Read"})
+    except(ReadException):
+        rows.append({"IP":device,"Comment":"Read"})
     except(EOFError):
-        offline.append(sw)
-        result = open("result.txt","a")
-        result.write(f"Error:{sw}:EOF error"+"\n")
-        result.close()
+        rows.append({"IP":device,"Comment":"EOF"})
+    
+    pbar.update(1)
 
-def main():
-    with tqdm(total=total,desc="Configuring devices") as pbar:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-            ejecucion = {executor.submit(connection,sw): sw for sw in devices}
-        for output in concurrent.futures.as_completed(ejecucion):
-            output.result()
-        pbar.update(1)
+tiempo1 = datetime.now()
+tiempo_inicial = tiempo1.strftime("%H:%M:%S")
+print(f"Hora de inicio: {tiempo_inicial}", f"Total de equipos a configurar: {str(len(devices))}", sep="\n")
 
-if __name__ == "__main__":
-    main()
+with tqdm(total=len(devices),desc="Configuring devices",colour="Blue") as pbar:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        ejecucion = {executor.submit(connection,device,pbar): device for device in devices}
+    for output in concurrent.futures.as_completed(ejecucion):
+        output.result()
 
-contador_out = len(offline)
 tiempo2 = datetime.now()
 tiempo_final = tiempo2.strftime("%H:%M:%S")
 tiempo_ejecucion = tiempo2 - tiempo1
-print(f"Hora de finalizacion: {tiempo_final}", f"Tiempo de ejecucion: {tiempo_ejecucion}", f"Total de equipos: {str(total)}",
-f"Total de equipos configurados: {str(len(configured))}",f"Total de equipos fuera: {str(contador_out)}",sep="\n")
+print(f"Hora de finalizacion: {tiempo_final}", 
+f"Tiempo de ejecucion: {tiempo_ejecucion}", 
+f"Total de equipos: {str(len(devices))}",
+f"Total de equipos configurados: {str(len(configc))}",
+f"Total de equipos fuera: {str(len(devices)-len(configc))}", sep="\n")
+
+writer.writerows(rows)
+swout.close()
