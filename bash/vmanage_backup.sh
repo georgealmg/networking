1d339f1f7643c7a0699187b3c945073345b14fb5
Jorge Marcano <71988888+georgealmg@users.noreply.github.com>
2026-02-24 14:49:59 -0300

    Update backup script to use absolute paths

--- bash/vmanage_backup.sh
+++ bash/vmanage_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date=$(date +%d%m%Y)
-mkdir ./$date
+mkdir /home/user/respaldos/SDWAN/$date
 
 cd vmanage_script
 for ip in "1.1.1.1" "1.1.1.2" "1.1.1.3" ; do
@@ -11,15 +11,15 @@ for ip in "1.1.1.1" "1.1.1.2" "1.1.1.3" ; do
     scp admin@$ip:/home/admin/confdb_backup.tar.gz /home/user/vmanage_script
     if [[ -f "confdb_backup.tar.gz" ]]; then
         if [[ "$ip" == "1.1.1.1" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_main01.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_main01.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         elif [[ $ip == "1.1.1.2" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_main02.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_main02.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         elif [[ $ip == "1.1.1.3" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_main03.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_main03.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         fi
@@ -35,15 +35,15 @@ for ip in "2.1.1.1" "2.1.1.2" "2.1.1.3" ; do
     scp admin@$ip:/home/admin/confdb_backup.tar.gz /home/user/vmanage_script
     if [[ -f "confdb_backup.tar.gz" ]]; then
         if [[ "$ip" == "2.1.1.1" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_standby01.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_standby01.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         elif [[ $ip == "2.1.1.2" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_standby02.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_standby02.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         elif [[ $ip == "2.1.1.3" ]]; then
-            mv confdb_backup.tar.gz respaldos/SDWAN/$date/confdb_backup_standby03.tar.gz
+            mv confdb_backup.tar.gz /home/user/respaldos/SDWAN/$date/confdb_backup_standby03.tar.gz
             echo "Eliminando backup en vManage_$ip..."
             ssh admin@$ip < vmanage_delete_backup.sh
         fi
